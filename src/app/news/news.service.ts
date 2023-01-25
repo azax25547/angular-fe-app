5117f68af79b54096bf59fe8ca6bb6f12598bdd4
Aditya Narayan Mishra <momentumangular@gmail.com>
2023-01-25 08:26:28 +0530

    ASCRAPPY-3 Read More feature enabled

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -11,14 +11,14 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   getNews(news: String): Observable<News> {
-    return this.http.get<News>(`http://localhost:4500/v1/api/ca/news/${news}`)
+    return this.http.get<News>(`https://panicky-handkerchief-hare.cyclic.app/v1/api/ca/news/${news}`)
   }
 
   getMoreNews(news: String, page: number): Observable<News> {
-    return this.http.get<News>(`http://localhost:4500/v1/api/ca/news/${news}?page=${page}`)
+    return this.http.get<News>(`https://panicky-handkerchief-hare.cyclic.app/v1/api/ca/news/${news}?page=${page}`)
   }
 
   getNewsContent(news: String, url: string): Observable<any> {
-    return this.http.post(`http://localhost:4500/v1/api/ca/news/${news}`, { url })
+    return this.http.post(`https://panicky-handkerchief-hare.cyclic.app/v1/api/ca/news/${news}`, { url })
   }
 }
